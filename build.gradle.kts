94c7fb23be5a95dc86ba623123b171cf2fbcb355
metehanie <metehan.bolat.ie@gmail.com>
2021-12-15 22:14:13 +0300

    Requests/Responses

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     application
     kotlin("jvm") version "1.5.31"
+    kotlin("plugin.serialization") version "1.5.31"
 }
 
 group = "com.metehanbolat"
@@ -20,5 +21,6 @@ dependencies {
     implementation("io.ktor:ktor-server-core:1.6.6")
     implementation("io.ktor:ktor-server-netty:1.6.6")
     implementation("ch.qos.logback:logback-classic:1.2.8")
+    implementation("io.ktor:ktor-serialization:1.6.6")
 
 }
--- src/main/kotlin/com/metehanbolat/Application.kt
+++ src/main/kotlin/com/metehanbolat/Application.kt
@@ -1,30 +1,58 @@
 package com.metehanbolat
 
 import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import io.ktor.serialization.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import kotlinx.serialization.Serializable
 
 fun main(){
     embeddedServer(Netty, port = 8080) {
+        install(ContentNegotiation){
+            json()
+        }
         module()
-        module2()
     }.start(wait = true)
 }
 
-fun Application.module(){
-    routing {
+fun Application.module() {
+    install(Routing) {
         get("/") {
             call.respondText("Hello World!")
+        }
+        get("/users/{username}") {
+            val userName = call.parameters["username"]
+            val header = call.request.headers["Connection"]
+            if (userName == "Admin"){
+                call.response.header(name = "CustomHeader", "Admin")
+                call.respond(message = "Hello Admin", status = HttpStatusCode.OK)
+            }
+            call.respondText("Greetings, $userName with header: $header")
+        }
+        get("/user") {
+            val name = call.request.queryParameters["name"]
+            val age = call.request.queryParameters["age"]
+            call.respondText("Hi, my name is $name, I'm $age years old!")
+        }
+        get("/person") {
+            val person = Person(name = "John", age = 26)
+            call.respond(message = person, status = HttpStatusCode.OK)
+        }
+        get("/redirect") {
+            call.respondRedirect(url = "/moved", permanent = false)
+        }
+        get("/moved") {
+            call.respondText("You have been successfully redirected!")
         }
     }
 }
 
-fun Application.module2(){
-    routing {
-        get("/book") {
-            call.respondText("Hello World 2!")
-        }
-    }
-}
+@Serializable
+data class Person(
+    val name: String,
+    val age: Int
+)
