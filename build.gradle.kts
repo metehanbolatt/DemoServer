6bcaab4cf1a1496027db41fb77b209dee99b5701
metehanie <metehan.bolat.ie@gmail.com>
2021-12-15 15:58:00 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    application
+    kotlin("jvm") version "1.5.31"
+}
+
+group = "com.metehanbolat"
+version = "0.0.1"
+
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+
+    implementation("io.ktor:ktor-server-core:1.6.6")
+    implementation("io.ktor:ktor-server-netty:1.6.6")
+    implementation("ch.qos.logback:logback-classic:1.2.8")
+
+}
